6572c99aa81409b2e439c9311e66b7d2d59cb498
Alejandro Vazquez <avazquez0191@gmail.com>
2025-04-22 16:03:33 -0400

    Remove unnecessary comments

--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -4,5 +4,5 @@ export const parseOrderFile = (filePath: string) => {
   const workbook = xlsx.readFile(filePath);
   const sheetName = workbook.SheetNames[0];
   const orders = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-  return orders; // Array of { 'Order ID': '123', 'Product': '...', ... }
+  return orders;
 };
